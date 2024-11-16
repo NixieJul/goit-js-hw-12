ab9f668f8d2b90d124db17a1b534b60d6d473169
NixieJul <nixie8821@gmail.com>
2024-11-16 02:21:02 +0200

    hw12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,41 +1,62 @@
+import axios from 'axios';
+
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = `46861005-d1ea17891df84db160fa13612`;
+const PER_PAGE = 15;
 
-const requestParams = new URLSearchParams({
-  key: API_KEY,
-  q: '',
-  image_type: 'photo',
-  orientation: 'horizontal',
-  safesearch: 'true',
-});
+async function fetchImages(query, page = 1) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: PER_PAGE,
+    page,
+  };
 
-const fetchImages = request => {
-  requestParams.set('q', request);
+  try {
+    const response = await axios.get(BASE_URL, { params });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw error;
+  }
+}
 
-  return fetch(`${BASE_URL}?${requestParams}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`Failed to fetch images`);
-      }
-
-      return response.json();
-    })
-    .then(response => {
-      if (response.hits.length === 0) {
-        throw new Error(
-          'Sorry, there are no images matching your search query. Please try again!'
-        );
-      }
-
-      return response.hits.map(hit => ({
-        webformatURL: hit.webformatURL,
-        largeImageURL: hit.largeImageURL,
-        tags: hit.tags,
-        likes: hit.likes,
-        views: hit.views,
-        comments: hit.comments,
-        downloads: hit.downloads,
-      }));
-    });
-};
 export { fetchImages };
+
+
+  
+  
+
+// const fetchImages = request => {
+//   requestParams.set('q', request);
+
+//   return fetch(`${BASE_URL}?${requestParams}`)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error(`Failed to fetch images`);
+//       }
+
+//       return response.json();
+//     })
+//     .then(response => {
+//       if (response.hits.length === 0) {
+//         throw new Error(
+//           'Sorry, there are no images matching your search query. Please try again!'
+//         );
+//       }
+
+//       return response.hits.map(hit => ({
+//         webformatURL: hit.webformatURL,
+//         largeImageURL: hit.largeImageURL,
+//         tags: hit.tags,
+//         likes: hit.likes,
+//         views: hit.views,
+//         comments: hit.comments,
+//         downloads: hit.downloads,
+//       }));
+//     });
+// };
+
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,36 +1,58 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-let lightbox;
-const galleryItem = ({
-  webformatURL,
-  largeImageURL,
-  tags,
-  likes,
-  views,
-  comments,
-  downloads,
-}) => {
-  return `<li class="image-card">
+
+const gallery = document.querySelector('.gallery');
+const moreBtn = document.querySelector('.more-btn');
+let lightbox = new SimpleLightbox('.gallery a');
+
+ export function showBtn() {
+  moreBtn.style.display = 'block';
+}
+export function hideBtn() {
+  moreBtn.style.display = 'none';
+}
+
+function renderGallery(images) {
+  const markup = images.map(({
+    webformatURL,
+    largeImageURL,
+    tags,
+    likes,
+    views,
+    comments,
+    downloads,
+  }) => `
+  <li class="image-card">
         <a href="${largeImageURL}">
         <img src="${webformatURL}" alt="${tags}">
         </a>
         <div class="image-info">
-                <span> Likes ${likes}</span>
-                <span> Comments ${comments}</span>
-                <span> Views ${views}</span>
-                <span> Downloads ${downloads}</span>
+                <p> Likes ${likes}</p>
+                <p> Comments ${comments}</p>
+                <p> Views ${views}</p>
+                <p> Downloads ${downloads}</p>
 </div>
-        </li>`;
-};
-const renderGallery = (gallery, images) => {
-  gallery.innerHTML = images.map(image => galleryItem(image)).join('');
+        </li>`)
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
 
-  if (!lightbox) {
-    lightbox = new SimpleLightbox('.gallery a');
-  } else {
-    lightbox.refresh();
-    }
+
+function clearGallery() {
+  gallery.innerHTML = '';
 };
 
+function scrollMore() {
+  const { height: cardHeight } =
+    gallery.firstElementChild.getBoundingClientRect();
+  window.scrollBy({
+    top: cardHeight,
+    behavior: 'smooth',
+  });
+}
+
+export { scrollMore };
+export { clearGallery };
 export { renderGallery };
